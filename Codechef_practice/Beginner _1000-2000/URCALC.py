39914308297628361d3f3566193fcbee3f7cfe7b
Aravindhan-Mathivanan <122088162+Aravindhan-Mathivanan@users.noreply.github.com>
2023-04-25 18:59:43 +0530

    Add files via upload

---
+++ Codechef_practice/Beginner _1000-2000/URCALC.py
@@ -0,0 +1,13 @@
+# cook your dish here
+A = int(input())
+B = int(input())
+C = input()
+
+if C == '+':
+    print(A+B)
+elif C == '-':
+    print(A-B)
+elif C == '*':
+    print(A*B)
+elif C == '/':
+    print(A/B)
