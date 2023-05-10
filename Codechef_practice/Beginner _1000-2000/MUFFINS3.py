93b683ca1d5f7b64183a55f2fa650c3c10583f7a
Aravindhan-Mathivanan <122088162+Aravindhan-Mathivanan@users.noreply.github.com>
2023-05-10 15:37:49 +0530

    Add files via upload

---
+++ Codechef_practice/Beginner _1000-2000/MUFFINS3.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for testcases in range(int(input())):
+    n = int(input())
+    n//=2
+# n//= 2 here n undergoes floor division by 2 and re-assign the result to n
+    print(n+1)
+            
